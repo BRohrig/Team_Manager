963807a7bf412074d12e5b468b795bf82fee2f6a
Brady Rohrig <brady.rohrig@gmail.com>
2022-12-01 20:40:56 -0800

    feat: completed up to user story 12

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: 'menu#main'
   get "/teams", to: 'teams#index'
+  get "/teams/new", to: 'teams#new'
+  post "/teams", to: 'teams#create'
   get "/teams/:id", to: 'teams#show'
   get "/players", to: 'players#index'
   get "/players/:id", to: 'players#show'
   get "/teams/:id/players", to: 'team_players#index'
+  
   
 end
